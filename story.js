3256f59a3de60930d2cefb2f1406d2706d4bbcd9
Aritro <aritro@Aritro-2.local>
2020-11-11 16:49:20 +0530

    Version 3

--- story.js
+++ story.js
@@ -1,8 +1,5 @@
 
-"Cinderella Story.Cinderella Fairytale Bedtime Story for Kids in English.This is the Fairytale story of Cinderella. It is brought to you by Stories to Grow by.ONCE UPON A TIME a girl named Cinderella lived with her stepmother and two stepsisters.  Poor Cinderella had to work hard all day long so the others could rest. It was she who had to wake up each morning when it was still dark and cold to start the fire.  It was she who cooked the meals. It was she who kept the fire going. The poor girl could not stay clean, from all the ashes and cinders by the fire. THIS IS A CHANGE TO THE STORY"
-
-
-
+"STORY VERSION 3"
 
 
 
